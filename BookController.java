d5dc392150aa3bb0eeec9fce9619a22692b4a7aa
Samuel Gómez <79483621+ExalMaelsu@users.noreply.github.com>
2024-07-16 00:32:26 -0600

    Create BookController.java

---
+++ BookController.java
@@ -0,0 +1,29 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    // Endpoint para buscar un libro por título
+    @GetMapping("/{title}")
+    public ResponseEntity<Book> findBookByTitle(@PathVariable String title) {
+        Book book = bookService.findBookByTitle(title);
+        if (book != null) {
+            return new ResponseEntity<>(book, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Puedes agregar más endpoints según sea necesario
+}
